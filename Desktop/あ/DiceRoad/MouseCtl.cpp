c49fe46a78c1190dc3c7e3fe7d45d8407f84607b
akazaki <yuyuko9903@icloud.com>
2018-11-02 12:55:55 +0900

    mouse追加

---
+++ Desktop/あ/DiceRoad/MouseCtl.cpp
@@ -0,0 +1,30 @@
+#include "Dxlib.h"
+#include "MouseCtl.h"
+
+
+MouseCtl::MouseCtl()
+{
+}
+
+
+MouseCtl::~MouseCtl()
+{
+}
+
+VECTOR2 MouseCtl::GetPoint() const
+{
+	return pos;
+}
+
+MOUSE_INT MouseCtl::GetBtn() const
+{
+	return mouseBtn;
+}
+
+void MouseCtl::Update()
+{
+	mouseBtn[ST_OLD] = mouseBtn[ST_NOW];
+	mouseBtn[ST_NOW] = GetMouseInput();
+
+	GetMousePoint(&pos.x, &pos.y);
+}
