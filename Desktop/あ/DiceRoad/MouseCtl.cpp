24d4e205a1e425229716b4f7dea04431064d7c74
akazaki <yuyuko9903@icloud.com>
2018-11-05 10:39:19 +0900

    DICE1.ipch

--- Desktop/あ/DiceRoad/MouseCtl.cpp
+++ Desktop/あ/DiceRoad/MouseCtl.cpp
@@ -1,35 +1,3 @@
-<<<<<<< .mine
-#include "MouseCtl.h"
-#include "DxLib.h"
-#include "ImageMng.h"
-
-MouseCtl::MouseCtl()
-{
-}
-
-
-MouseCtl::~MouseCtl()
-{
-}
-
-void MouseCtl::UpDate(void)
-{
-	mouseBtn[ST_OLD] = mouseBtn[ST_NOW];		// ŒÃ‚¢î•ñ‚ğOld‚É•Û‘¶‚·‚è‚ã
-	mouseBtn[ST_NOW] = GetMouseInput();
-	GetMousePoint(&pos.x, &pos.y);
-}
-
-VECTOR2 MouseCtl::GetPoint(void) const
-{
-	return pos;
-}
-
-MOUSE_INT MouseCtl::GetBtn(void) const
-{
-	return mouseBtn;
-}
-
-=======
 #include "Dxlib.h"
 #include "MouseCtl.h"
 
@@ -60,4 +28,3 @@ void MouseCtl::Update()
 
 	GetMousePoint(&pos.x, &pos.y);
 }
->>>>>>> .theirs
