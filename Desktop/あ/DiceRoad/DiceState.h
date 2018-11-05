b67bacfd0b5e56b70f3fda541fc92ecbee20890f
akazaki <yuyuko9903@icloud.com>
2018-11-05 17:06:31 +0900

    追加

---
+++ Desktop/あ/DiceRoad/DiceState.h
@@ -0,0 +1,23 @@
+#pragma once
+
+enum DICE_ST {
+	DICE_NON,
+	DICE_1,
+	DICE_2,
+	DICE_3,
+	DICE_4,
+	DICE_5,
+	DICE_6,
+	DICE_BLOCK,
+	DICE_MAX
+};
+
+class DiceState
+{
+public:
+	//DiceState();
+	virtual ~DiceState();
+	virtual DICE_ST GetState(void) = 0;
+	virtual void DrawDice(void) = 0;
+};
+
